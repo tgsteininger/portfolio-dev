0f31bea0d18f1eef6ab744b5da86b99493e640de
tgsteininger <tgsteininger@gmail.com>
2026-04-16 03:01:15 -0700

    Analytics fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { IBM_Plex_Sans, Fira_Sans } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react'
 import './globals.css'
-import { Analytics } from "@vercel/analytics/next";
 
 const ibmPlexSans = IBM_Plex_Sans({
   subsets: ['latin'],
